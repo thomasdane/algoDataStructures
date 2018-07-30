563bf5f3b32b05fd8ef368b931c91526d71517dc
Thomas Dane <tjdane@gmail.com>
2018-07-31 00:23:33 +0100

    Add algos folder

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Test inputs
 
-let empty = []; // then []
-let oneItem = [3]; // then [3]
-let twoItems = [4,3]; //then [3,4]
-let threeItems = [3, 5, 4]; // then [3,4,5]
-let fiveItems = [3, 5, 4, 1, 2]; // then [1,2,3,4,5]
-let negative = [-3, -5, -4]; // then [-5,-4,-3]
+// let empty = []; // then []
+// let oneItem = [3]; // then [3]
+// let twoItems = [4,3]; //then [3,4]
+// let threeItems = [3, 5, 4]; // then [3,4,5]
+// let fiveItems = [3, 5, 4, 1, 2]; // then [1,2,3,4,5]
+// let negative = [-3, -5, -4]; // then [-5,-4,-3]
 
 /////////////////// SORTING //////////////////////////
 
@@ -22,11 +22,13 @@ let negative = [-3, -5, -4]; // then [-5,-4,-3]
 //////////////// DATA STRUCTURES ////////////////////
 
 // NODE
+
 // import Node from './dataStructures/node.js';
 // let node = new Node(2);
 // console.log(node);
 
 // LINKED LIST
+
 import LinkedList from './dataStructures/linkedList.js';
 
 let list = new LinkedList();
@@ -35,8 +37,8 @@ list.push(1);
 list.push(2);
 list.push(3);
 list.push(4);
-
 console.log(list)
+
 let value = list._find(2);
 console.log(value);
 
