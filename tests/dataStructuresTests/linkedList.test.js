5bcb230e5490344e56883f0e48b9f313d213a0b9
Thomas Dane <tjdane@gmail.com>
2018-08-01 00:38:29 +0100

    Add test for merge sort and list push

---
+++ tests/dataStructuresTests/linkedList.test.js
@@ -0,0 +1,35 @@
+import LinkedList from '../../dataStructures/linkedList.js';
+
+test('Push_WhenEmptyList_ThenUpdatesHead', () => {
+    let emptyList = new LinkedList();
+    emptyList.push(1);
+    expect(emptyList.head.value).toEqual(1);
+});
+
+test('Push_WhenEmptyList_ThenUpdatesHead', () => {
+    let emptyList = new LinkedList();
+    emptyList.push(1);
+    expect(emptyList.tail.value).toEqual(1);
+});
+
+test('Push_WhenNotEmptyList_ThenUpdatesTail', () => {
+    let list = new LinkedList();
+    
+    list.push(1);
+    list.push(2);
+    
+    expect(list.tail.value).toEqual(2);
+});
+
+test('Find_WhenValueExists_ReturnsTrue', () => {
+    //Arrange
+    let list = new LinkedList();
+    
+    //Act
+    list.push(1);
+    list.push(2);
+
+    //Assert
+    expect(list._find(2, list.head)).toEqual(2);
+});
+
