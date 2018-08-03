3ea1a6d5ad51108ffecda9df9cb27074e4dbcb04
Thomas Dane <tjdane@gmail.com>
2018-08-03 23:26:11 +0100

    Clean up

--- tests/dataStructuresTests/linkedLists/push.test.js
+++ tests/dataStructuresTests/linkedLists/push.test.js
@@ -2,13 +2,17 @@ import LinkedList from '../../../dataStructures/linkedList.js';
 
 test('Push_WhenEmptyList_ThenUpdatesHead', () => {
     let emptyList = new LinkedList();
+
     emptyList.push(1);
+    
     expect(emptyList.head.value).toEqual(1);
 });
 
 test('Push_WhenEmptyList_ThenUpdatesHead', () => {
     let emptyList = new LinkedList();
+
     emptyList.push(1);
+
     expect(emptyList.tail.value).toEqual(1);
 });
 
