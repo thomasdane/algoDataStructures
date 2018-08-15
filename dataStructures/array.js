56c28f4f45aab80027c76d2a15dfa28e153538d8
Thomas Dane <tjdane@gmail.com>
2018-08-15 11:36:52 +0200

    Arrays - use delete for pop

--- dataStructures/array.js
+++ dataStructures/array.js
@@ -12,9 +12,8 @@ export class ArrayList{
     }
 
     pop() {
-        var result = this.data[this.length - 1];
-        this.data[this.length - 1] = null;
-        this.length--;
+        const result = this.get(this.length -1)
+        this.delete(this.length - 1);
         return result;
     }
 
@@ -22,8 +21,26 @@ export class ArrayList{
        return this.data[index]; 
     }
 
-    delete(index){
-        delete this.data[index];
+    delete(index) {
+        if(index > this.length) return;
+
+        for (let i = index; i < this.length; i++) 
+        {
+            this.data[i] = this.data[i+1];
+        }
+
+        this.data[this.length-1] = null;
         this.length--;
     }
+
+    //[a,b,c,d]
+    //collapse 1
+    //first loop
+    //i = 1, length = 4
+    //whatever is at 1 becomes what is at 2
+    //second last loop, whatever is at 2 becomes 3
+    //last loop
+    //whatever is at 3, becomes null
+    //so final result is [a,c,d,null]
+    //then we delete the end
 }
