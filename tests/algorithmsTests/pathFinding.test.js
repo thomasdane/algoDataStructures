4b63e6c2a150b48a0a1a78bd1aa5c4534dba2669
Tom Dane <tjdane@gmail.com>
2018-10-13 09:18:14 -0700

    Get neighbours without walls complete

--- tests/algorithmsTests/pathFinding.test.js
+++ tests/algorithmsTests/pathFinding.test.js
@@ -8,15 +8,43 @@ describe("getNeighbours", function() {
       [0, 0, 0, 2]
     ];
 
-    it("should return 4 neighbours", () => {
-        //arrange
-        const point = fourByFour[1][1];
-        const expected = [0, 0, 0, 0];
+    describe("when no edges or walls", () => {
+        it("should return 4 neighbours", () => {
+            //arrange
+            const point = [1,1];
+            const expected = [[1,0],[0,1],[1,2],[2,1]];
+    
+            //act
+            const actual = getNeighbours(fourByFour, point[0], point[1]);
+    
+            //assert
+            expect(actual).toEqual(expected);
+        });
+    });
 
-        //act
-        const actual = getNeighbours(fourByFour, point);
+    describe("when edge", () => {
+        it("should return 2 neighbours for top left", () => {
+            //arrange
+            const point = [0,0];
+            const expected = [[0,1],[1,0]];
 
-        //assert
-        expect(actual).toEqual(expected);
+            //act
+            const actual = getNeighbours(fourByFour, point[0], point[1]);
+
+            //assert
+            expect(actual).toEqual(expected);
+        });
+
+        it("should return 2 neighbours for bottom right", () => {
+            //arrange
+            const point = [3,3];
+            const expected = [[3,2],[2,3]];
+
+            //act
+            const actual = getNeighbours(fourByFour, point[0], point[1]);
+
+            //assert
+            expect(actual).toEqual(expected);
+        });
     });
 });
