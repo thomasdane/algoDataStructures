759e137990898593a5c80081ab9f79e08cd98cca
Tom Dane <tjdane@gmail.com>
2018-10-30 11:03:48 -0700

    Can print minefield with hidden bombs

--- katas/mineSweeper.js
+++ katas/mineSweeper.js
@@ -1,40 +1,86 @@
-//game
-
-//generate grid
-
-const generateGrid = (size, bombCount) => {
-    
-    const result = [];
-
-    for(let i = 0; i < size; i++) {
-        const row = new Array(size).fill(0);
-        result.push(row);
+class Square {
+    constructor(value, row, column){
+        this.hidden = true;
+        this.value = value;
+        this.row = row;
+        this.column = column;
     }
+}
 
-    return result;
+const getRandomIndex = size => Math.floor(Math.random() * size);
+const createRow = (size, row) => createSquares(size, row);
+
+const createSquares = (size, row) => {
+    const squares = [];
+    for(i = 0; i < size; i++){
+        const square = new Square(0, row, i);
+        squares.push(square);
+    }
+    return squares;
+}
+
+const generateGrid = (size) => {
+    const grid = [];
+    for(let i = 0; i < size; i++) {
+        const row = createRow(size, i);
+        grid.push(row);
+    }
+    return grid;
 }
 
 const generateBombPositions = (size, bombCount) => {
-    const result = [];
-    const row = Math.floor(Math.random() * size + 1);
-    const column = Math.floor(Math.random() * size);
-    const callBack = () => console.log(row, column)
-    timesDo(bombCount, callBack);
-    return result;
+    const bombs = [];
+    for(let i = 0; i < bombCount; i++) {
+        const bomb = generateBombPosition(size);
+        bombs.push(bomb);
+    }
+    return bombs;
 }
 
-const timesDo = (times, callback)=> {
-    while(times-- > 0) callback();
+const generateBombPosition = size => {
+    const row = getRandomIndex(size);
+    const column = getRandomIndex(size);
+    return [row, column];
 }
+
+const generateGridWithBombs = (grid, bombs) => {
+    const temp = grid.map(row => [...row]);
+
+    bombs.forEach(bomb => {
+        row = bomb[0];
+        column = bomb[1];
+        temp[row][column].value = '!';
+    });
+
+    return temp;
+}
+
 
 const printGrid = grid => {
-    grid.forEach(row => console.log(row));
+    grid.forEach(row => {
+        const result = [];
+
+        row.forEach(square => {
+            if(square.hidden === true) {
+                console.log(square)
+                result.push('#')
+            } else {
+                console.log(square)
+                result.push(square.value)
+            }
+        });
+
+        console.log(result);
+    });
 }   
 
 const grid = generateGrid(4);
 const bombs = generateBombPositions(4, 4);
-printGrid(grid);
-console.log(bombs)
+const gridWithBombs = generateGridWithBombs(grid, bombs);
+
+//printGrid(grid);
+//console.log(bombs)
+printGrid(gridWithBombs);
 
 //show grid
 
