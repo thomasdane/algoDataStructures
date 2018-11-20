d88bed2ffaee85d0d864fc29b0a0ecd905ca3e47
Tom Dane <tjdane@gmail.com>
2018-11-20 12:10:31 -0800

    Add busiest time at mall kata

---
+++ katas/pramp/busiestMallTime.js
@@ -0,0 +1,113 @@
+function findBusiestPeriod(data) {
+    
+    let maxCount = 0;
+    let existingCount = 0;
+    let matchingIndex = 0;
+
+    const periods = data;
+    
+    periods.forEach((currentPeriod, index) => {
+
+        let nextPeriod = periods[index+1] || [];
+        let nextPeriodTimestamp = nextPeriod[0];
+
+        let currentPeriodTimeStamp = currentPeriod[0];
+        let currentPeriodCount = currentPeriod[1];
+        let currentPeriodAction = currentPeriod[2];
+
+        existingCount = updateCount(existingCount, currentPeriodCount, currentPeriodAction);
+        
+        if(isNewTimePeriod(currentPeriodTimeStamp, nextPeriodTimestamp)) {
+            if(isNewMax(existingCount, maxCount)){
+                matchingIndex = index;
+                maxCount = existingCount;
+            } 
+        } 
+    });
+
+    const matchingPeriod = data[matchingIndex];
+    const matchingTimeStamp = matchingPeriod[0];
+    return matchingTimeStamp;
+}
+
+function isNewMax(existingCount, maxCount) {
+    return existingCount > maxCount;
+}
+
+function isNewTimePeriod(currentTimeStamp, nextTimeStamp) {
+    return currentTimeStamp !== nextTimeStamp;
+}
+
+function updateCount(previous, current, action){
+    
+    let result = previous;
+
+    if(action === 0) {
+        result -= current;
+
+    } else {
+        result += current;
+    }
+    return result;
+}
+
+function updateCount1(previous, current, action){
+    
+    const isEnter = action === 1;
+    const add = previous += current;
+    const subtract = previous -= current;
+    return isEnter ? add : subtract;
+}
+
+//exit = 0, enter = 1
+
+let input = [
+    // 0 
+    [1487799425, 14, 1], // +14 
+    [1487799425, 4,  0], // -4
+    [1487799425, 2,  0], // -2
+    // 8
+    [1487800378, 10, 1], // +10
+    // 18
+    [1487801478, 18, 0], // -18
+    [1487801478, 18, 1], // +18
+    //18
+    [1487901013, 1,  0], // -1
+    //17
+    [1487901211, 7,  1], // +7
+    [1487901211, 7,  0]  // -7
+]   //17
+           
+let expected = 1487800378;         
+let actual = findBusiestPeriod(input);
+console.log(expected === actual);
+
+input = [[1487799426,21,1]]
+expected = 1487799426
+actual = findBusiestPeriod(input);
+console.log(expected === actual);
+ 
+input = [[1487799425,21,0],[1487799427,22,1],[1487901318,7,0]]
+expected = 1487799427
+actual = findBusiestPeriod(input);
+console.log(expected === actual);
+ 
+input = [[1487799425,21,1],[1487799425,4,0],[1487901318,7,0]]
+expected = 1487799425
+actual = findBusiestPeriod(input);
+console.log(expected === actual);
+ 
+input = [[1487799425,14,1],[1487799425,4,0],[1487799425,2,0],[1487800378,10,1],[1487801478,18,0],[1487801478,18,1],[1487901013,1,0],[1487901211,7,1],[1487901211,7,0]]
+expected = 1487800378
+actual = findBusiestPeriod(input);
+console.log(expected === actual);
+ 
+input = [[1487799425,14,1],[1487799425,4,1],[1487799425,2,1],[1487800378,10,1],[1487801478,18,1],[1487901013,1,1],[1487901211,7,1],[1487901211,7,1]]
+expected = 1487901211
+actual = findBusiestPeriod(input);
+console.log(expected === actual);
+ 
+input = [[1487799425,14,1],[1487799425,4,0],[1487799425,2,0],[1487800378,10,1],[1487801478,18,0],[1487801478,19,1],[1487801478,1,0],[1487801478,1,1],[1487901013,1,0],[1487901211,7,1],[1487901211,8,0]]
+expected = 1487801478
+actual = findBusiestPeriod(input);
+console.log(expected === actual);
