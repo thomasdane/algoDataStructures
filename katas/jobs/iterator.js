9669969894c48ec3a1f8bbf0c38ed2df4533bebe
Tom Dane <tjdane@gmail.com>
2018-11-21 13:23:26 -0800

    Add recursive iterator

--- katas/jobs/iterator.js
+++ katas/jobs/iterator.js
@@ -34,7 +34,25 @@ class Iterator {
             }
         }
     
-        return result.join();
+        return result.join("").replace(/,/g, "");
+    }
+
+    iterateRecurse(input) {
+
+        function recurse(input, output) {
+
+            if(input.length === 0) return output;
+    
+            input.forEach(element => {
+                if(Array.isArray(element)) return recurse(element, output);
+                output.push(element);
+            });
+
+            return output;
+        }
+
+        const result = recurse(input, []);
+        return result.join("");
     }
 }
 
@@ -43,22 +61,49 @@ const emptyInput = [];
 const singleElementInput = [1];
 const twoElementsInput = [1,2];
 const nestedInput = [1,[2,3]];
-const doublyNestedInput = [1,[2,[3,4],5],6]
 const sampleInput = [9,[1,3],4,5];
+const doublyNestedInput = [1,[2,[3,4],5],6]
+const triplyNestedInput = [1,[2,[3,[4, 5],6],7],8]
+const emptyNestedInput = [1,[2,[3,[],6],7],8]
+
 
 //Act
-const iterator = new Iterator();
-const empty = iterator.iterate(emptyInput);
-const singleElement = iterator.iterate(singleElementInput);
-const twoElements = iterator.iterate(twoElementsInput);
-const nested = iterator.iterate(nestedInput);
-const doublyNested = iterator.iterate(doublyNestedInput);
-const sample = iterator.iterate(sampleInput);
+let iterator = new Iterator();
+let empty = iterator.iterate(emptyInput);
+let singleElement = iterator.iterate(singleElementInput);
+let twoElements = iterator.iterate(twoElementsInput);
+let nested = iterator.iterate(nestedInput);
+let sample = iterator.iterate(sampleInput);
+let doublyNested = iterator.iterate(doublyNestedInput);
+let triplyNested = iterator.iterate(triplyNestedInput);
+let emptyNested = iterator.iterate(emptyNestedInput);
 
 //Assert
 console.log(empty.length === 0);
 console.log(singleElement === "1");
-console.log(twoElements === "1,2");
-console.log(nested === "1,2,3");
-console.log(sample === "9,1,3,4,5");
-console.log(doublyNested === "1,2,3,4,5,6");
+console.log(twoElements === "12");
+console.log(nested === "123");
+console.log(sample === "91345");
+console.log(doublyNested === "123456");
+console.log(triplyNested === "12345678");
+console.log(emptyNested === "123678");
+
+// iterator = new Iterator();
+// empty = iterator.iterateRecurse(emptyInput);
+// singleElement = iterator.iterateRecurse(singleElementInput);
+// twoElements = iterator.iterateRecurse(twoElementsInput);
+// nested = iterator.iterateRecurse(nestedInput);
+// sample = iterator.iterateRecurse(sampleInput);
+// doublyNested = iterator.iterateRecurse(doublyNestedInput);
+// triplyNested = iterator.iterateRecurse(triplyNestedInput);
+// emptyNested = iterator.iterateRecurse(emptyNestedInput);
+
+// //Assert
+// console.log(empty.length === 0);
+// console.log(singleElement === "1");
+// console.log(twoElements === "12");
+// console.log(nested === "123");
+// console.log(sample)// === "91345");
+// console.log(doublyNested)// === "123456");
+// console.log(triplyNested === "12345678");
+// console.log(emptyNested === "123678");
---
+++ katas/pramp/cracking.js
@@ -0,0 +1,139 @@
+/*
+Design and implement an iterator for a heterogenous list, where each element may be an int or a list of ints.
+The iterator should iterate over the ints in order they appear in the list, including traversing the embedded lists.
+
+Example input: [9, [1, 3], 4, 5]
+
+Expected iteration order: 9,1,3,4,5
+
+The iterator should have the following methods:
+
+// Are there are elements left in the iteration?
+public boolean hasNext();
+
+// Return the next element in the iteration, or fail if there are no more elements.
+public int next(); 
+
+*/
+
+
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class List{
+  constructor(){
+    this.head = null;
+    this.tail = null;
+  }
+  
+  push(node) {  
+        if(!this.head) {
+            this.head = node;
+            this.tail = node; 
+
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+    }
+}
+
+/*
+Intersection: Given two (singly) linked lists, determine if the two lists intersect. Return the intersecting
+node. Note that the intersection is defined based on reference, not value. That is, if the kth
+node of the first linked list is the exact same node (by reference) as the jth node of the second
+linked list, then they are intersecting.
+*/
+
+const a = ['a'];
+const b = ['b'];
+const c = ['c'];
+const d = ['d'];
+const e = ['e'];
+const f = ['a'];
+
+//console.log("reference", a === f);
+
+const listA = new List();
+listA.push(a);
+listA.push(b);
+
+const listB = new List();
+listB.push(b);
+listB.push(c);
+
+const listC = new List();
+listC.push(d);
+listC.push(e);
+
+const listOne = new List();
+var one = new Node(1);
+var two = new Node(2);
+listOne.push(one);
+listOne.push(two);
+
+const listTwo = new List();
+var three = new Node(3);
+listTwo.push(two);
+listTwo.push(three);
+
+const listThree = new List();
+var twoA = new Node(2);
+var four = new Node(4);
+listThree.push(twoA);
+listThree.push(four);
+
+
+
+function isIntersection(listA, listB){
+  
+  const references = new Set();
+  
+  function populateSet(node){
+    if(!node) return;
+    
+    references.add(node);
+    
+    return populateSet(node.next);
+  }
+  
+  populateSet(listB.head);
+  
+  function recurse(node) {
+    if(!node) return false;
+    
+    if(references.has(node)){
+      return true;
+    }
+    
+    return recurse(node.next);
+  }
+  
+  
+  return recurse(listA.head)
+  
+  
+}
+/*
+const success = isIntersection(listA, listB);
+console.log(success);
+
+const failure = isIntersection(listA, listC);
+console.log(failure);*/
+
+const successInt = isIntersection(listOne, listTwo);
+console.log(successInt);
+
+const failureInt = isIntersection(listOne, listThree);
+console.log(failureInt);
+
+// 1,2
+// 2
+
+
+
