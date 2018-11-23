b1baaea4f4ab42549388e0b5669d4c710c3ba3e2
Tom Dane <tjdane@gmail.com>
2018-11-23 15:39:06 -0800

    Iterative binary search

--- algorithms/search/binarySearch.js
+++ algorithms/search/binarySearch.js
@@ -17,6 +17,29 @@ function binarySearch(needle, haystack) {
     return recurse(needle, haystack, 0, haystack.length - 1)
 }
 
+function binarySearchIterative(needle, haystack) {
+
+    let lowerBound = 0;
+    let upperBound = haystack.length - 1;
+    
+    while(lowerBound <= upperBound){
+
+        const middle = (upperBound - lowerBound)/2;
+        const middleIndex = Math.floor(middle + lowerBound);
+        const middleElement = haystack[middleIndex];
+
+        if(needle === middleElement) return middleIndex;
+
+        if(needle < middleElement) {
+            upperBound = middleIndex - 1
+        } else {
+            lowerBound = middleIndex + 1;
+        }
+    }
+
+    return -1;
+}
+
 let needle = 5;
 let haystack = [1,2,3,4,5];
 let expected = 4;
@@ -33,4 +56,22 @@ needle = 5;
 haystack = [1,2,3];
 expected = -1;
 actual = binarySearch(needle, haystack);
+console.log(actual === expected);
+
+needle = 5;
+haystack = [1,2,3,4,5];
+expected = 4;
+actual = binarySearchIterative(needle, haystack);
+console.log(actual === expected);
+
+needle = 'a';
+haystack = ['a','b','c'];
+expected = 0;
+actual = binarySearchIterative(needle, haystack);
+console.log(actual === expected);
+
+needle = 5;
+haystack = [1,2,3];
+expected = -1;
+actual = binarySearchIterative(needle, haystack);
 console.log(actual === expected);
